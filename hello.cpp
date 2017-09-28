5eb54da3965ffc92aebf27089412f142bdc91c2c
Rishabh Verma <rishabhverma17@gmail.com>
2017-09-28 12:01:13 +0530

    Fix

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void)
 {
   cout << "Hello, World\n";
-  cout<<"If You can read this, It means its build successfully."
+  cout<<"If You can read this, It means its build successfully.";
   return 0;
 }
