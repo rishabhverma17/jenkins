6929bee85d691eece9752c8fc1e0fab580488adc
Rishabh Verma <rishabhverma17@gmail.com>
2017-09-28 11:59:18 +0530

    Edit

--- hello.cpp
+++ hello.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main(void)
 {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-
-  cout << "I have successfully built and run\n";
+  cout<<"If You can read this, It means its build successfully."
   return 0;
 }
